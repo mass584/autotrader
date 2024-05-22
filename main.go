7429c78d2b607b8e1e1ffc451543910fa520bf63
yoshimitsu.masugata <msgtysmt@gmail.com>
2024-05-23 00:10:30 +0900

    dbとのコネクション作成をmain関数で行うようにする

--- main.go
+++ main.go
@@ -4,14 +4,17 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/mass584/autotrader/config"
 	"github.com/mass584/autotrader/entity"
 	"github.com/mass584/autotrader/exchange/bitflyer"
 	"github.com/mass584/autotrader/exchange/coincheck"
 	"github.com/mass584/autotrader/repository"
 	"github.com/mass584/autotrader/trade_algorithms"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
-func ScrapingFromCoinCheck() {
+func ScrapingFromCoinCheck(db *gorm.DB) {
 	startID := 1       // TODO データベースの値から決めるようにする
 	endID := 264330000 // TODO APIで取得した値から決めるようにする
 	count := endID - startID + 1
@@ -21,11 +24,22 @@ func ScrapingFromCoinCheck() {
 	for page := 0; page < pageMax; page++ {
 		lastId := startID + page*per + per - 1
 		tradeCollection := coincheck.GetAllTradesByLastId(entity.BTC_TO_JPY, lastId)
-		repository.SaveTrades(tradeCollection)
+		repository.SaveTrades(db, tradeCollection)
 	}
 }
 
 func main() {
+	config, err := config.NewConfig()
+	if err != nil {
+		return
+	}
+	db, err := gorm.Open(mysql.Open(config.DatabaseURL()))
+	if err != nil {
+		return
+	}
+
+	ScrapingFromCoinCheck(db)
+
 	orderBookBitflyer := bitflyer.GetOrderBook(entity.BTC_TO_JPY)
 	tradesBitflyer := bitflyer.GetRecentTrades(entity.BTC_TO_JPY)
 	orderPriceBitflyer := trade_algorithms.DetermineOrderPrice(orderBookBitflyer, tradesBitflyer.RecentTrades(5*time.Minute))
@@ -35,6 +49,4 @@ func main() {
 	tradesCoinCheck := coincheck.GetRecentTrades(entity.BTC_TO_JPY)
 	orderPriceCoinCheck := trade_algorithms.DetermineOrderPrice(orderBookCoinCheck, tradesCoinCheck.RecentTrades(5*time.Minute))
 	fmt.Printf("Determined Order Price at Coincheck: %.2f [JPY/BTC]\n", orderPriceCoinCheck)
-
-	ScrapingFromCoinCheck()
 }
--- repository/trade_repository.go
+++ repository/trade_repository.go
@@ -1,22 +1,12 @@
 package repository
 
 import (
-	"github.com/mass584/autotrader/config"
 	"github.com/mass584/autotrader/entity"
-	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"gorm.io/gorm/clause"
 )
 
-func SaveTrades(tradeCollection entity.TradeCollection) {
-	config, err := config.NewConfig()
-	if err != nil {
-		return
-	}
-	db, err := gorm.Open(mysql.Open(config.DatabaseURL()))
-	if err != nil {
-		return
-	}
+func SaveTrades(db *gorm.DB, tradeCollection entity.TradeCollection) {
 	db.Clauses(clause.OnConflict{
 		Columns:   []clause.Column{{Name: "exchange_name"}, {Name: "trade_id"}},
 		DoUpdates: clause.AssignmentColumns([]string{"price", "volume", "time"}),
