6c44fe2a8ae28a1e4de5fe0239c503a52ab40b98
yoshimitsu.masugata <msgtysmt@gmail.com>
2024-05-23 11:24:02 +0900

    main関数の実装をサービス層に移動

--- main.go
+++ main.go
@@ -2,61 +2,42 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"os"
 
 	"github.com/mass584/autotrader/config"
-	"github.com/mass584/autotrader/entity"
-	"github.com/mass584/autotrader/repository/database"
-	"github.com/mass584/autotrader/repository/external/bitflyer"
-	"github.com/mass584/autotrader/repository/external/coincheck"
-	"github.com/mass584/autotrader/trade_algorithms"
+	"github.com/mass584/autotrader/service"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
-func ScrapingFromCoinCheck(db *gorm.DB) {
-	startID := 240000000 // TODO データベースの値から決めるようにする
-	endID := 264330000   // TODO APIで取得した値から決めるようにする
-	count := endID - startID + 1
-	per := 50
-	pageMax := (count+1)/per + 1
-
-	for page := 0; page < pageMax; page++ {
-		lastId := startID + page*per + per - 1
-		time.Sleep(100 * time.Millisecond)
-		tradeCollection := coincheck.GetAllTradesByLastId(entity.BTC_TO_JPY, lastId)
-		database.SaveTrades(db, tradeCollection)
-	}
-}
-
 func main() {
+	args := os.Args
+	if len(args) != 2 {
+		fmt.Printf("Invalid argument.")
+		return
+	}
+
 	config, err := config.NewConfig()
 	if err != nil {
+		fmt.Printf("Invalid config.")
 		return
 	}
+
 	db, err := gorm.Open(mysql.Open(config.DatabaseURL()))
 	if err != nil {
+		fmt.Printf("Failed to connect database.")
 		return
 	}
 
-	// このリポジトリは取引所のサーバーからスクレイピングする実装になっているので、
-	// 50件しか取得できないため、移動平均を計算するのに十分なコレクションを取得することができない。
-	// データベースに永続化したストアから取得するリポジトリに差し替える必要がある。
-	tradeCollection := coincheck.GetAllTradesByLastId(entity.BTC_TO_JPY, 264330000)
-	trendSignal, _ := trade_algorithms.TrendFollowingSignal(tradeCollection)
-	fmt.Printf("TrandFollowSignal is: %s\n", trendSignal)
-	meanReversionSignal, _ := trade_algorithms.MeanReversionSignal(tradeCollection)
-	fmt.Printf("MeanReversionSignal is: %s\n", meanReversionSignal)
-
-	ScrapingFromCoinCheck(db)
-
-	orderBookBitflyer := bitflyer.GetOrderBook(entity.BTC_TO_JPY)
-	tradesBitflyer := bitflyer.GetRecentTrades(entity.BTC_TO_JPY)
-	orderPriceBitflyer := trade_algorithms.DetermineOrderPrice(orderBookBitflyer, tradesBitflyer.RecentTrades(5*time.Minute))
-	fmt.Printf("Determined Order Price at Bitflyer: %.2f [JPY/BTC]\n", orderPriceBitflyer)
-
-	orderBookCoinCheck := coincheck.GetOrderBook(entity.BTC_TO_JPY)
-	tradesCoinCheck := coincheck.GetRecentTrades(entity.BTC_TO_JPY)
-	orderPriceCoinCheck := trade_algorithms.DetermineOrderPrice(orderBookCoinCheck, tradesCoinCheck.RecentTrades(5*time.Minute))
-	fmt.Printf("Determined Order Price at Coincheck: %.2f [JPY/BTC]\n", orderPriceCoinCheck)
+	mode := args[1]
+	switch mode {
+	case "scraping":
+		service.ScrapingTradesFromCoincheck(db)
+	case "order_price":
+		service.DetermineOrderPrice()
+	case "trade_signal":
+		service.CalculateTradeSignal()
+	default:
+		fmt.Printf("Invalid execution mode.")
+	}
 }
